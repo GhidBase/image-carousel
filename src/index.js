d0f95cd960820770f1e891d3c36de5ae47325fd6
Dylan <grvdylans@gmail.com>
2025-01-23 00:40:29 -0700

    Remove console logs

--- src/index.js
+++ src/index.js
@@ -11,7 +11,6 @@ class ImageCarousel {
         for (let image of this.slider.children) {
             this.imgWidths.push(image.offsetWidth)
         }
-        console.log(this.imgWidths);
     }
 
     moveToXImage(amountToMove) {
@@ -19,13 +18,12 @@ class ImageCarousel {
         let amountToMovePx = 0;
         for (let i = 0; i <= amountToMove; i++) {
             amountToMovePx += this.imgWidths[i];
-            console.log(amountToMovePx);
         }
         amountToMovePx = amountToMovePx - this.imgWidths[amountToMove]/2;
-        console.log(amountToMovePx)
         this.slider.style.left = "-" + amountToMovePx + "px";
     }
 }
 
 let carousel = new ImageCarousel(".slider");
+window.carousel = carousel;
 carousel.moveToXImage(7)
